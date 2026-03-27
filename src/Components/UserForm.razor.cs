5d009f59a5732e36d9f9e8fe22352eb86915539f
Matt Lagat <mattinfotech23@gmail.com>
2026-03-27 17:07:56 +0800

    Isolate the code behind to separate .cs file

---
+++ src/Components/UserForm.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlazorTestProject.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTestProject.Components
+{
+    public partial class UserForm
+    {
+        [Parameter, EditorRequired]
+        public UserModel Model { get; set; } = default!;
+
+        [Parameter]
+        public string SubmitButtonText { get; set; } = "Submit";
+
+        [Parameter]
+        public bool IsSubmitting { get; set; }
+
+        [Parameter, EditorRequired]
+        public EventCallback<UserModel> OnSubmit { get; set; }
+
+        private async Task HandleValidSubmit()
+        {
+            await OnSubmit.InvokeAsync(Model);
+        }
+    }
+}
